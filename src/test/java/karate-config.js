e5a5188d3745b52a39ad7180b69fc6c65c41f256
Juan Cruz Segura <jsegura@MacBook-Pro-de-wolox-7.local>
2020-03-18 12:53:11 -0300

    karate-config basic setup template

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -4,15 +4,37 @@ function fn() {
   if (!env) {
     env = 'dev';
   }
+
   var config = {
-    env: env,
-	myVarName: 'someValue'
+    env: env
   }
-  if (env == 'dev') {
-    // customize
-    // e.g. config.foo = 'bar';
-  } else if (env == 'e2e') {
-    // customize
+
+  // use this when extra env data configuration is needed, adding the file with the info to classpath
+  //    var data = read('classpath:env-' + env);
+  //    var env_data = data[env];
+
+  switch (env) {
+    case 'dev':
+        // customize
+        break;
+    case 'stage':
+        // customize
+        break;
+    case 'prod':
+        // customize
+        break;
   }
+
+  karate.log('karate running now tests on env:', env);
+
+  karate.configure('connectTimeout', 5000);
+  karate.configure('readTimeout', 5000);
+
+  karate.configure('retry', { count: 3, interval: 30000 });
+
+  // might set to false for tests with performance issues
+  karate.configure('logPrettyRequest', true);
+  karate.configure('logPrettyResponse', true);
+
   return config;
 }
